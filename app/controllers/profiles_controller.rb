a3cbf6b96a3d08f4bd41c35a34afc78ecc4cd8c7
Sergey Kalistratov <me@sergeykalistratov.com>
2017-03-22 18:54:14 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
   
   # GET to /users/:user_id/profile/new
   def new
@@ -51,5 +53,10 @@ class ProfilesController < ApplicationController
                                       :phone_number,
                                       :contact_email,
                                       :description)
+    end
+    
+    def only_current_user
+      @user = User.find(params[:user_id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
